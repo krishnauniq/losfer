fa3e5fc4e70933e8e85b2576e0916bfa2eb062ed
krishnachoubey314@gmail.com <krishnachoubey314@gmail.com>
2026-01-26 13:27:49 +0530

    Security: Use environment variables for Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBs-UGtLXB3Xr1wmM3QT3rA-h1VSM-cwqM",
-  authDomain: "losfer-1b785.firebaseapp.com",
-  projectId: "losfer-1b785",
-  storageBucket: "losfer-1b785.firebasestorage.app",
-  messagingSenderId: "36222500314",
-  appId: "1:36222500314:web:cc352fcea399dfceded4e3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
